2f063c862fa81287f93fbb9f772362c291576919
josejosemarcosribeiro-alt <josejosemarcosribeiro@gmail.com>
2025-11-26 15:56:56 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    CLOUDINARY_URL:
+      "https://api.cloudinary.com/v1_1/dpnhg6v2z/image/upload",
+    CLOUDINARY_URL_VID:
+      "https://api.cloudinary.com/v1_1/dpnhg6v2z/video/upload",
+    CLOUDINARY_CLOUD_NAME: "dpnhg6v2z",
+    CLOUDINARY_UPLOAD_PRESET: "exodo999",
+    MONGO_URI:
+      "mongodb+srv://codigox:9%25%25d.7w%25%25Vpr%23FA@cluster0.xjtg379.mongodb.net/?appName=Cluster0",
+    JWT_SECRET: "F3r@123xYz!9LmNoP",
+    SENDGRID_API:
+      "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",
+  },
+};
