0addaf18980f179c020b0f94e22942b179f00811
josejosemarcosribeiro-alt <josejosemarcosribeiro@gmail.com>
2025-11-26 17:19:02 -0500

    Create exodo999.jsx

---
+++ exodo999.jsx
@@ -0,0 +1,16 @@
+// pages/exodo999.jsx
+export default function Exodo999() {
+  return (
+    <div style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh", 
+      fontSize: "48px", 
+      fontFamily: "Arial, sans-serif",
+      color: "#333"
+    }}>
+      Olá Mundo
+    </div>
+  );
+}
